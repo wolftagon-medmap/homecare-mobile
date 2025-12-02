dd8bbd7752b77851e64a5ef80dba5db82afeccb4
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-12-02 14:25:52 +0700

    fix: unauthorized interceptor issue when failed to login

--- lib/features/auth/injection.dart
+++ lib/features/auth/injection.dart
@@ -2,11 +2,13 @@ import 'package:get_it/get_it.dart';
 import 'package:m2health/features/auth/data/datasources/google_auth_source.dart';
 import 'package:m2health/features/auth/data/repositories/auth_repository.dart';
 import 'package:m2health/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:dio/dio.dart';
 
 void initAuthModule(GetIt sl) {
   // Repository
   sl.registerLazySingleton(
-    () => AuthRepository(dio: sl(), googleAuthSource: sl()),
+    () => AuthRepository(dio: Dio(), googleAuthSource: sl()),
+    // Instantiate Dio for preventing unauthorized interceptor issues
   );
 
   // Data Sources
