f4927e4578509c0e17938881e3cbc7c6751524bf
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-12-15 19:46:14 +0700

    feat: auth for caregiver role

--- lib/const.dart
+++ lib/const.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Const {
-  // static const String BASE_URL = 'http://192.168.0.114:3333';
+  // static const String BASE_URL = 'http://127.0.0.1:3333';
   static const String BASE_URL = 'https://homecare-api.med-map.org';
   static const String URL_API = BASE_URL + '/v1';
   static const String URL_IMG_PLACEHOLDER = 'https://placehold.co/100x100';
